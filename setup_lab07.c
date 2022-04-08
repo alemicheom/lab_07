8a11c28cf5065c97b810db63c6815fef067a8189
alemicheom <mic20735@uvg.edu.gt>
2022-04-07 23:08:42 -0600

    lab_07_setup_final

--- setup_lab07.c
+++ setup_lab07.c
@@ -12,8 +12,11 @@ void setup(void){
     ANSEL = 0;
     ANSELH = 0;                 // I/O digitales
     
-    OSCCONbits.IRCF = 0b0101;   // 2MHz
-    OSCCONbits.SCS = 1;         // Oscilador interno
+    OSCCONbits.IRCF = 0b0110; // 4MHz
+    OSCCONbits.SCS = 1;       // Oscilador interno
+    
+    //OSCCONbits.IRCF = 0b0101;   // 2MHz
+    //OSCCONbits.SCS = 1;         // Oscilador interno
     
     TRISA = 0;                  // PORTA salida
     PORTA = 0; 
@@ -24,7 +27,16 @@ void setup(void){
     // contador tmr0
     TRISC = 0;                  // PORTC salida
     PORTC = 0; 
-   
+    
+    // contador displays
+    TRISD = 0;                  // PORTD salida
+    PORTD = 0; 
+    
+     // banderas displays
+    TRISE = 0;                  // PORTD salida
+    PORTE = 0; 
+    
+    
     // TMR0
     OPTION_REGbits.T0CS = 0;
     OPTION_REGbits.PSA  = 0;
